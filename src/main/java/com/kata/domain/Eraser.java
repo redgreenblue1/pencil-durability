8de363e3551ede302d8b1d02b29a349889c124e3
redgreenblue1 <44486177+redgreenblue1@users.noreply.github.com>
2018-10-26 13:39:47 -0400

    Refactor Eraser class

--- src/main/java/com/kata/domain/Eraser.java
+++ src/main/java/com/kata/domain/Eraser.java
@@ -18,14 +18,22 @@ public class Eraser {
 
 
     public String erase(String inputText, String textToErase) {
-        int lastIndexOfTextToEars = inputText.lastIndexOf(textToErase);
+        int lastIndexOfTextToErase = inputText.lastIndexOf(textToErase);
         char[] inputTextCharacters = inputText.toCharArray();
-        for (int i = lastIndexOfTextToEars + textToErase.length() - 1; i >= lastIndexOfTextToEars; i--) {
-            if (!Character.isWhitespace(inputTextCharacters[i]) && getDurability() != 0) {
-                inputTextCharacters[i] = ' ';
-                this.durability--;
+        for (int i = lastIndexOfTextToErase + textToErase.length() - 1; i >= lastIndexOfTextToErase; i--) {
+            if (canErase(inputTextCharacters[i])) {
+                handleErase(inputTextCharacters, i);
             }
         }
         return String.copyValueOf(inputTextCharacters);
+    }
+
+    protected boolean canErase(char inputTextCharacter) {
+        return !Character.isWhitespace(inputTextCharacter) && getDurability() != 0;
+    }
+
+    protected void handleErase(char[] inputTextCharacters, int i) {
+        inputTextCharacters[i] = ' ';
+        this.durability--;
     }
 }
